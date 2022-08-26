1b7be574e40e97086165a508efc4d6235c30fb37
Rachel Drapeau <67067228+redrapeau@users.noreply.github.com>
2022-08-25 22:04:32 -0600

    completed boundary problem

--- binary_search/finding_the_boundary.py
+++ binary_search/finding_the_boundary.py
@@ -11,3 +11,26 @@ Output: 2
 Explanation: first true's index is 2.
 '''
 
+from typing import List
+
+def find_boundary(arr: List[bool]) -> int:
+    left_idx, right_idx = 0, len(arr)-1
+    boundary_idx = -1
+    
+    while left_idx <= right_idx:
+        mid_idx = (left_idx + right_idx)//2
+        
+        if arr[mid_idx] is True:
+            boundary_idx = mid_idx
+            right_idx = mid_idx - 1
+            
+        else:
+            left_idx = mid_idx + 1
+            
+    return boundary_idx
+    
+
+if __name__ == '__main__':
+    arr = [x == "true" for x in input().split()]
+    res = find_boundary(arr)
+    print(res)
